8379db899464da70c3f377876d279f03e29b9d6d
Bachynin Ivan <bachynin.i@gmail.com>
2017-08-06 01:00:39 +0300

    ✨ add mock-server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './in-memory-data.service';
+
+import {AppRoutingModule} from './app-routing.module';
 
 import {AppComponent} from './app.component';
 import {HeroDetailsComponent} from './hero-details/hero-details.component';
 import {HeroesComponent} from './heroes/heroes.component';
 import {HeroService} from './hero.service';
 import {DashboardComponent} from './dashboard/dashboard.component';
-import {AppRoutingModule} from './app-routing.module';
 
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
         AppRoutingModule,
     ],
     declarations: [
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,17 +1,31 @@
 import {Injectable} from '@angular/core';
+import {Headers, Http} from '@angular/http';
 
-import {HEROES} from './mock-heroes';
+import 'rxjs/add/operator/toPromise';
+
 import {Hero} from './hero';
 
 
 @Injectable()
 export class HeroService {
-    getHeroes(): Promise<Hero[]> {
-        return Promise.resolve(HEROES);
+    private heroesUrl = 'api/heroes';
+
+    constructor(private http: Http) {
     }
 
-    getHero(id: number): Promise<Hero> {
-        return this.getHeroes()
-            .then(heroes => heroes.find(hero => hero.id === id));
+    getHeroes(): Promise<Hero[]> {
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(response => response.json().data as Hero[])
+            .catch(this.handleError);
+    }
+
+    getHero(id: number) {
+        return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error);
+        return Promise.reject(error.message || error);
     }
 }
