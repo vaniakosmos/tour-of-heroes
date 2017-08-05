a7b164bd04b49661e10911f89d0fa54fe28a88ad
Bachynin Ivan <bachynin.i@gmail.com>
2017-08-05 15:32:53 +0300

    ✨ add some hero data and two-way binding for hero's name and input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import {Component} from '@angular/core';
 
+export class Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.sass']
 })
+
 export class AppComponent {
-    title = 'My First Angular App';
+    title = 'Tour of Heroes';
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
 }
